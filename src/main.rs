7ca3390df3eae57e8430db73bea947141823cea8
PygmalesDev <uk084094@student.uni-kassel.de>
2024-12-08 13:10:24 +0100

    implemented selecting physical device with graphics queue family

--- src/main.rs
+++ src/main.rs
@@ -213,7 +213,19 @@ extern "system" fn debug_callback(
 pub struct SuitabilityError(pub &'static str);
 
 unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
-    Ok(())
+    for physical_device in instance.enumerate_physical_devices()? {
+        let properties = instance.get_physical_device_properties(physical_device);
+
+        if let Err(error) = check_physical_device(instance, data, physical_device) {
+            warn!("Skipping physical device (`{:?}`):  {}", properties.device_name, error);
+        } else {
+            info!("Selected physical device (`{}`).", properties.device_name);
+            data.physical_device = physical_device;
+            return Ok(());
+        }
+    }
+
+    Err(anyhow!("Failed to find suitable physical device."))
 }
 
 unsafe fn check_physical_device(
@@ -224,9 +236,40 @@ unsafe fn check_physical_device(
     let properties = instance.get_physical_device_properties(physical_device);
     let features = instance.get_physical_device_features(physical_device);
     
+    QueueFamilyIndices::get(instance, data, physical_device)?;
+
     if VALIDATION_ENABLED {
         debug!("{:?} ({:?})", properties.device_name, properties.device_type);
     }
 
     Ok(())
+}
+
+
+#[derive(Copy, Clone, Debug)]
+struct QueueFamilyIndices {
+    graphics: u32,
+}
+
+impl QueueFamilyIndices {
+    unsafe fn get(
+        instance: &Instance,
+        data: &AppData,
+        physical_device: vk::PhysicalDevice,
+    ) -> Result<Self> {
+        let properties = instance.
+            get_physical_device_queue_family_properties(physical_device);
+
+        let graphics = properties
+            .iter()
+            .position(|p| p.queue_flags.contains(vk::QueueFlags::GRAPHICS))
+            .map(|i| i as u32);
+
+        if let Some(graphics) = graphics {
+            Ok(Self { graphics })
+        } else {
+            Err(anyhow!(SuitabilityError("Missing required queue families.")))
+        }
+
+    }
 }
